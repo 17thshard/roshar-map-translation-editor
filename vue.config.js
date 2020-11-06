f0cc600c8dbab86b4ac1c5816308bb1863a04b51
Marvin Rösch <marvinroesch99@gmail.com>
2020-11-06 19:54:37 +0100

    Build portable target for Windows

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       preload: 'src/preload.js',
+      win: {
+        target: 'portable'
+      },
       mac: {
         hardenedRuntime: true,
         entitlements: 'build/entitlements.mac.plist',
